87fb9acde3fdfd7ec7afa810624e12ca3f27dd68
rganjare <rahul.ganjare@gmail.com>
2022-06-13 00:56:44 +0530

    initial commit

---
+++ vars/sample.groovy
@@ -0,0 +1,8 @@
+def lintChecks() {
+      sh '''
+        # We commented this because devs gonna check the failures.
+        #~/node_modules/jslint/bin/jslint.js server.js
+        echo Link Check 
+        #for ${COMPONENT}
+      '''
+    }
