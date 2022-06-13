f16c502775e6119688486be0a194e118e4761121
rganjare <rahul.ganjare@gmail.com>
2022-06-13 18:18:24 +0530

    Added shared library

---
+++ vars/maven.groovy
@@ -0,0 +1,24 @@
+def lintChecks() {
+      sh '''
+        # We commented this because devs gonna check the failures.
+        #~/node_modules/jslint/bin/jslint.js server.js
+        mvn checkstyle:check
+        echo Link Check for ${COMPONENT}
+      '''
+    }
+
+def call() {
+pipeline {
+   agent any
+
+   stages{
+       stage ("Lint Check"){
+           steps {
+               script{
+                 lintChecks()
+               }
+           }  
+       }
+   }
+}
+}
