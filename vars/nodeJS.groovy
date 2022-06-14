f20da665f9f4c20786231e474272c7c51b981d9a
rganjare <rahul.ganjare@gmail.com>
2022-06-14 19:01:54 +0530

    Added shared library

--- vars/nodeJS.groovy
+++ vars/nodeJS.groovy
@@ -57,18 +57,24 @@ pipeline {
         }
       
       stage("Prepare Artifacts"){
-              steps{
-                sh 'echo Prepare Artifacts'
-              }
-            }
+         when {
+          expression {env.TAG_NAME != null}
+         } 
+          steps{
+            sh 'echo Prepare Artifacts'
+          }
+        }
 
       stage("Upload Artifacts"){
-              steps{
-                sh 'echo Upload Artifacts'
-              }
-            }
+          when {
+          expression {env.TAG_NAME != null}
+         } 
+          steps{
+            sh 'echo Upload Artifacts'
+          }
+        }
 
-            
+
     } 
   }
 }
