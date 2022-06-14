f5582cb7144528ab6d932d20d1d175041587b741
rganjare <rahul.ganjare@gmail.com>
2022-06-14 18:46:30 +0530

    Added shared library

--- vars/common.groovy
+++ vars/common.groovy
@@ -1,7 +1,7 @@
 def sonarCheck() {
       sh '''
-          sonar-scanner -Dsonar.host.url=http://172.31.15.120:9000 -Dsonar.projectKey=${COMPONENT} -Dsonar.login=${SONAR_USR} ${ARGS}
-          sleep 5
+          #sonar-scanner -Dsonar.host.url=http://172.31.15.120:9000 -Dsonar.projectKey=${COMPONENT} -Dsonar.login=${SONAR_USR} ${ARGS}
+          #sleep 5
           #sonar-quality-gate.sh ${SONAR_USR} 172.31.15.120 ${COMPONENT}
           echo Sonar Checks for ${COMPONENT}
       '''
