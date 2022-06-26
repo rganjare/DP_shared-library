f7002b6e63fc9f3c6891732b3dc14ca2deb4eaa8
rganjare <rahul.ganjare@gmail.com>
2022-06-26 22:16:23 +0530

    updated

--- vars/docker.groovy
+++ vars/docker.groovy
@@ -2,10 +2,12 @@ def call() {
   node {
     sh 'rm -rf *'
     git branch: 'main', url: "https://github.com/rganjare/${COMPONENT}"
-    env.APP_TYPE = "NodeJS"
     
     stage("Docker Build"){
-      sh 'docker build .'
+      sh '''
+          pwd
+          $USER 
+          docker build .'''
     }
   }
 }
