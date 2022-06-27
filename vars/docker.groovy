7730f0764b13c552ba0cfac4f095ee23c601493e
rganjare <rahul.ganjare@gmail.com>
2022-06-27 11:38:37 +0530

    updated

--- vars/docker.groovy
+++ vars/docker.groovy
@@ -6,19 +6,19 @@ def call() {
     stage("Docker Build"){
      // sh "sudo docker build -t 549008638695.dkr.ecr.us-east-1.amazonaws.com/${DOCKER_COMPONENT}:latest ."
       // Creating image for Docker hub
-      sh "sudo docker build -t rganjaredocker/${DOCKER_COMPONENT}:${TAG_NAME} ."
+      sh "sudo docker build -t rganjaredocker/${COMPONENT1}:${TAG_NAME} ."
     }
 
-    if (env.TAG_NAME != null) {
-      stage('Docker Build') {
-        sh """
-         ### Pushing image into docker hub
-         #docker tag 549008638695.dkr.ecr.us-east-1.amazonaws.com/${DOCKER_COMPONENT}:latest rganjaredocker/${DOCKER_COMPONENT}:${TAG_NAME}
-          docker login --username rganjaredocker --password Rahul#143
-          docker push rganjaredocker/${DOCKER_COMPONENT}:${TAG_NAME}
-        """
-      }
-    }
+    // if (env.TAG_NAME != null) {
+    //   stage('Docker Build') {
+    //     sh """
+    //      ### Pushing image into docker hub
+    //      #docker tag 549008638695.dkr.ecr.us-east-1.amazonaws.com/${DOCKER_COMPONENT}:latest rganjaredocker/${DOCKER_COMPONENT}:${TAG_NAME}
+    //       docker login --username rganjaredocker --password Rahul#143
+    //       docker push rganjaredocker/${DOCKER_COMPONENT}:${TAG_NAME}
+    //     """
+    //   }
+    // }
 
   }
 }
