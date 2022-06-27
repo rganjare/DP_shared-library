87866df417e4d7d090c4ec5be834b6c92aeaf79a
rganjare <rahul.ganjare@gmail.com>
2022-06-27 10:16:15 +0530

    updated

--- vars/docker.groovy
+++ vars/docker.groovy
@@ -4,7 +4,22 @@ def call() {
     git branch: 'main', url: "https://github.com/rganjare/${COMPONENT}"
     
     stage("Docker Build"){
-      sh 'sudo docker build . '
+      sh "sudo docker build -t 549008638695.dkr.ecr.us-east-1.amazonaws.com/${COMPONENT}:latest ."
     }
+
+    if (env.TAG_NAME != null) {
+      stage('Docker Build') {
+        sh """
+          docker tag 549008638695.dkr.ecr.us-east-1.amazonaws.com/${COMPONENT}:latest 549008638695.dkr.ecr.us-east-1.amazonaws.com/${COMPONENT}:${TAG_NAME}
+          (Get-ECRLoginCommand).Password | docker login --username AWS --password-stdin 549008638695.dkr.ecr.us-east-1.amazonaws.com
+          docker push 549008638695.dkr.ecr.us-east-1.amazonaws.com/${COMPONENT}:${TAG_NAME}
+          
+          docker tag 549008638695.dkr.ecr.us-east-1.amazonaws.com/${COMPONENT}:latest rganjaredocker/${COMPONENT}:${TAG_NAME}
+          docker login --username rganjaredocker --password Rahul#143"
+          docker push rganjaredocker/${COMPONENT}:${TAG_NAME}
+        """
+      }
+    }
+
   }
 }
