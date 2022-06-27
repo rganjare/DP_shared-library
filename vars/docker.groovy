a9752773d8ab436b7439a9f256b9e6e43e21740c
rganjare <rahul.ganjare@gmail.com>
2022-06-27 22:11:19 +0530

    updated

--- vars/docker.groovy
+++ vars/docker.groovy
@@ -3,14 +3,17 @@ def call() {
     sh 'rm -rf *'
     git branch: 'main', url: "https://github.com/rganjare/${COMPONENT}"
     
-    // stage("Docker Build"){
-    //   sh "sudo docker build -t 549008638695.dkr.ecr.us-east-1.amazonaws.com/${dockerCOMPONENT}:latest ."
-    // }
-
-    if (env.TAG_NAME != null) {
+   if (env.TAG_NAME == null) {
+    stage("Docker Build"){
+        sh """
+          sudo docker build -t rganjaredocker/${dockerCOMPONENT}:latest .
+          docker login --username rganjaredocker --password Rahul#143
+          docker push rganjaredocker/${dockerCOMPONENT}:latest
+        """
+    }
+   else {
       stage('Docker Build') {
         sh """
-         ### Pushing image into docker hub
           sudo docker build -t rganjaredocker/${dockerCOMPONENT}:${TAG_NAME} .
           docker login --username rganjaredocker --password Rahul#143
           docker push rganjaredocker/${dockerCOMPONENT}:${TAG_NAME}
