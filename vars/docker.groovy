fe16477d05085f0456b509543c922733250af362
rganjare <rahul.ganjare@gmail.com>
2022-06-27 18:24:30 +0530

    updated

--- vars/docker.groovy
+++ vars/docker.groovy
@@ -4,21 +4,20 @@ def call() {
     git branch: 'main', url: "https://github.com/rganjare/${COMPONENT}"
     
     stage("Docker Build"){
-     // sh "sudo docker build -t 549008638695.dkr.ecr.us-east-1.amazonaws.com/${DOCKER_COMPONENT}:latest ."
+      sh "sudo docker build -t 549008638695.dkr.ecr.us-east-1.amazonaws.com/${dockerCOMPONENT}:latest ."
       // Creating image for Docker hub
       sh "sudo docker build -t rganjaredocker/${dockerCOMPONENT}:${TAG_NAME} ."
     }
 
-    // if (env.TAG_NAME != null) {
-    //   stage('Docker Build') {
-    //     sh """
-    //      ### Pushing image into docker hub
-    //      #docker tag 549008638695.dkr.ecr.us-east-1.amazonaws.com/${DOCKER_COMPONENT}:latest rganjaredocker/${DOCKER_COMPONENT}:${TAG_NAME}
-    //       docker login --username rganjaredocker --password Rahul#143
-    //       docker push rganjaredocker/${DOCKER_COMPONENT}:${TAG_NAME}
-    //     """
-    //   }
-    // }
+    if (env.TAG_NAME != null) {
+      stage('Docker Build') {
+        sh """
+         ### Pushing image into docker hub
+          docker login --username rganjaredocker --password Rahul#143
+          docker push rganjaredocker/${dockerCOMPONENT}:${TAG_NAME}
+        """
+      }
+    }
 
   }
 }
