617d91b11ad9ff396be8561a7627795d16faafc7
rganjare <rahul.ganjare@gmail.com>
2022-06-27 18:50:31 +0530

    updated

--- vars/docker.groovy
+++ vars/docker.groovy
@@ -10,15 +10,10 @@ def call() {
     if (env.TAG_NAME != null) {
       stage('Docker Build') {
         sh """
-         docker tag 549008638695.dkr.ecr.us-east-1.amazonaws.com/${dockerCOMPONENT}:latest 549008638695.dkr.ecr.us-east-1.amazonaws.com/${dockerCOMPONENT}:${TAG_NAME}
-        (Get-ECRLoginCommand).Password | docker login --username AWS --password-stdin 549008638695.dkr.ecr.us-east-1.amazonaws.com
-         docker push 549008638695.dkr.ecr.us-east-1.amazonaws.com/${dockerCOMPONENT}:${TAG_NAME}
-
-
          ### Pushing image into docker hub
           sudo docker build -t rganjaredocker/${dockerCOMPONENT}:${TAG_NAME} .
           docker login --username rganjaredocker --password Rahul#143
-          docker push rganjaredocker/${dockerCOMPONENT}:${env.TAG_NAME}
+          docker push rganjaredocker/${dockerCOMPONENT}:${TAG_NAME}
         """
       }
     }
